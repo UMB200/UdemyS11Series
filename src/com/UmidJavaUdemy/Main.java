22c28e2d4d92f6abe8e50bf668f40de371b86286
UMB200 <tomodachi200@gmail.com>
2020-01-25 11:50:57 -0500

    final commit

---
+++ src/com/UmidJavaUdemy/Main.java
@@ -0,0 +1,13 @@
+package com.UmidJavaUdemy;
+
+import com.example.series.Series;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Series.factorial(6);
+        Series.fibonacci(6);
+        System.out.println("Fibonacci: "+ Series.nSum(6));
+
+    }
+}
